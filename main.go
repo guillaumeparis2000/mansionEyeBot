24dc5d793c7ec34a0f11f4b4f548b8299b92bf47
Guillaume Paris <guillaume.paris.work@gmail.com>
2019-05-20 16:02:04 +0200

    :art: Refactor to use the new version of tbot and add multiple user authentication

--- main.go
+++ main.go
@@ -11,58 +11,91 @@ import (
 	"time"
 )
 
+type application struct {
+	client *tbot.Client
+}
+
+var validUsers = []string{"user1", "user2"}
+
+func contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-	bot, err := tbot.NewServer(os.Getenv("TELEGRAM_TOKEN"))
+	bot := tbot.New(os.Getenv("TELEGRAM_TOKEN"))
+	app := &application{}
+
+	// Use validUsers for Auth middleware, allow to interact only with user1 and user2
+	bot.Use(app.auth)
+
+	app.client = bot.Client()
+	bot.HandleMessage("/status", app.statusHandler)
+	bot.HandleMessage("/pause", app.pauseHandler)
+	bot.HandleMessage("/resume", app.resumeHandler)
+	bot.HandleMessage("/check", app.checkHandler)
+	bot.HandleMessage("/time", app.timeHandler)
+	bot.HandleMessage("/video", app.videoHandler)
+	bot.HandleMessage("/snapshot", app.snapShotHandler)
+
+	err := bot.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Use whitelist for Auth middleware, allow to interact only with user1 and user2
-	whitelist := []string{"youUser"}
-	bot.AddMiddleware(tbot.NewAuth(whitelist))
-
-	bot.HandleFunc("/status", statusHandler)
-	bot.HandleFunc("/pause", pauseHandler)
-	bot.HandleFunc("/resume", resumeHandler)
-	bot.HandleFunc("/check", checkHandler)
-	bot.HandleFunc("/time", timeHandler)
-	bot.HandleFunc("/video", videoHandler)
-	bot.HandleFunc("/snapshot", snapShotHandler)
-	bot.ListenAndServe()
 }
 
-func statusHandler(m *tbot.Message) {
+func (a *application) auth(h tbot.UpdateHandler) tbot.UpdateHandler {
+	return func(u *tbot.Update) {
+		if contains(validUsers, u.Message.Chat.Username) {
+			h(u)
+		} else {
+			a.client.SendMessage(u.Message.Chat.ID, "You are not allowed to use this Bot!")
+		}
+	}
+}
+
+func (a *application) statusHandler(m *tbot.Message) {
 	result := webControl("detection", "status")
-	m.Reply(result)
+	a.client.SendChatAction(m.Chat.ID, tbot.ActionTyping)
+	a.client.SendMessage(m.Chat.ID, result)
 }
 
-func pauseHandler(m *tbot.Message) {
+func (a *application) pauseHandler(m *tbot.Message) {
 	result := webControl("detection", "pause")
-	m.Reply(result)
+	a.client.SendChatAction(m.Chat.ID, tbot.ActionTyping)
+	a.client.SendMessage(m.Chat.ID, result)
 }
 
-func resumeHandler(m *tbot.Message) {
+func (a *application) resumeHandler(m *tbot.Message) {
 	result := webControl("detection", "start")
-	m.Reply(result)
+	a.client.SendChatAction(m.Chat.ID, tbot.ActionTyping)
+	a.client.SendMessage(m.Chat.ID, result)
 }
 
-func checkHandler(m *tbot.Message) {
+func (a *application) checkHandler(m *tbot.Message) {
 	result := webControl("detection", "connection")
-	m.Reply(result)
+	a.client.SendChatAction(m.Chat.ID, tbot.ActionTyping)
+	a.client.SendMessage(m.Chat.ID, result)
 }
 
-func snapShotHandler(m *tbot.Message) {
+func (a *application) snapShotHandler(m *tbot.Message) {
 	result := webControl("action", "snapshot")
-	m.Reply(result)
+	a.client.SendChatAction(m.Chat.ID, tbot.ActionUploadPhoto)
+	a.client.SendMessage(m.Chat.ID, result)
 }
 
-func timeHandler(m *tbot.Message) {
+func (a *application) timeHandler(m *tbot.Message) {
 	result := time.Now().Format(time.ANSIC)
-	m.Reply(result)
+	a.client.SendChatAction(m.Chat.ID, tbot.ActionTyping)
+	a.client.SendMessage(m.Chat.ID, result)
 }
 
-func videoHandler(m *tbot.Message) {
-	m.Reply("This operation can be long! Please wait.")
+func (a *application) videoHandler(m *tbot.Message) {
+	a.client.SendMessage(m.Chat.ID, "This operation can be long! Please wait.")
 	dir := `/var/lib/motioneye/Camera1/`
 
 	files, _ := ioutil.ReadDir(dir)
@@ -80,10 +113,14 @@ func videoHandler(m *tbot.Message) {
 
 	if len(videos) > 0 {
 		var lastVideo = dir + videos[0]
-		m.Reply("Uploading Video, Please wait a little more. Thanks")
-		m.ReplyVideo(lastVideo)
+		a.client.SendMessage(m.Chat.ID, "Uploading Video, Please wait a little more. Thanks")
+		a.client.SendChatAction(m.Chat.ID, tbot.ActionUploadVideo)
+		_, err := a.client.SendVideoFile(m.Chat.ID, lastVideo)
+		if err != nil {
+			a.client.SendMessage(m.Chat.ID, err.Error())
+		}
 	} else {
-		m.Reply("No video available!")
+		a.client.SendMessage(m.Chat.ID, "No video available!")
 	}
 }
 
